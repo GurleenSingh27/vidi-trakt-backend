9b1ee2f666dabfd5e9d91f52c6b6e57ee05b9564
GurleenSingh27 <94331732+GurleenSingh27@users.noreply.github.com>
2025-08-17 14:44:20 -0700

    Delete api directory

--- api/echo.js
+++
@@ -1,6 +0,0 @@
-export const config = { runtime: 'edge' };
-export default async function handler(req) {
-  return new Response(JSON.stringify({ ok: true, method: req.method }, null, 2), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
