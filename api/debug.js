2f9dce2954590611ae94e67bc3125765c668d4e9
GurleenSingh27 <94331732+GurleenSingh27@users.noreply.github.com>
2025-08-17 11:35:09 -0700

    Add files via upload

---
+++ api/debug.js
@@ -0,0 +1,8 @@
+export const config = { runtime: 'edge' };
+export default async function handler() {
+  return new Response(JSON.stringify({
+    allowed_origin: process.env.ALLOWED_ORIGIN || null,
+    has_client_id: !!process.env.TRAKT_CLIENT_ID,
+    has_client_secret: !!process.env.TRAKT_CLIENT_SECRET,
+  }, null, 2), { headers: { 'Content-Type': 'application/json' } });
+}
